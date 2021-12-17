760b63f38f2bd671ed248ea8d1ebee16561321ca
lisoh <sohyunlee0902@gmail.com>
2021-12-17 09:23:32 +0900

    refactor: CommentList 컴포넌트 분리
    Co-authored-by: Taehee Kim <rabolution@gmail.com>

--- src/realworld/pages/ArticleDetail.tsx
+++ src/realworld/pages/ArticleDetail.tsx
@@ -1,78 +1,38 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router";
+import CommentList from "../components/CommentList";
 import NavigationBar from "../components/NavigationBar";
-
-interface CommentCardProps {
-  body: string;
-  author: {
-    username: string;
-    image: string;
-  };
-  createdAt: string;
-}
-
-function CommentCard({ body, author, createdAt }: CommentCardProps) {
-  return (
-    <div className="card">
-      <div className="card-block">
-        <p className="card-text">{body}</p>
-      </div>
-      <div className="card-footer">
-        <a className="comment-author" href={"#@" + author.username}>
-          <img src={author.image} className="comment-author-img" />
-        </a>
-        &nbsp;
-        <a className="comment-author" href={"#@" + author.username}>
-          {author.username}
-        </a>
-        <span className="date-posted">
-          {new Date(createdAt).toLocaleDateString("ko-kr")}
-        </span>
-      </div>
-    </div>
-  );
-}
 
 function ArticleDetail() {
   const { slug } = useParams();
 
-  const {
-    isLoading: isArticleLoading,
-    error: articleError,
-    data: article,
-  } = useQuery<Article, Error>("Article/" + slug, () =>
-    fetch(`https://api.realworld.io/api/articles/${slug}`)
-      .then((res) => res.json())
-      .then((body) => body.article)
+  const { isLoading, error, data } = useQuery<Article, Error>(
+    "Article/" + slug,
+    () =>
+      fetch(`https://api.realworld.io/api/articles/${slug}`)
+        .then((res) => res.json())
+        .then((body) => body.article)
   );
 
-  const {
-    isLoading: isCommentsLoading,
-    error: commentsError,
-    data: commentList,
-  } = useQuery<Array<CommentData>, Error>("Comments/" + slug, () =>
-    fetch(`https://api.realworld.io/api/articles/${slug}/comments`)
-      .then((res) => res.json())
-      .then((body) => body.comments)
-  );
-
-  if (isArticleLoading || isCommentsLoading) {
+  if (isLoading) {
     return <span>로딩 중</span>;
   }
 
-  if (articleError instanceof Error || commentsError instanceof Error) {
+  if (error instanceof Error) {
     return (
       <span>
-        에러가 발생했습니다. {articleError?.message} {commentsError?.message}
+        에러가 발생했습니다.
+        {error?.message}
       </span>
     );
   }
-  if (article === undefined) {
+
+  if (data === undefined) {
     return <span>아티클이 없습니다!</span>;
   }
 
-  const authorName = article.author.username;
+  const authorName = data.author.username;
 
   return (
     <div data-reactroot="">
@@ -80,17 +40,17 @@ function ArticleDetail() {
       <div className="article-page">
         <div className="banner">
           <div className="container">
-            <h1>{article.title}</h1>
+            <h1>{data.title}</h1>
             <div className="article-meta">
               <a className="" href={"#@" + authorName}>
-                <img src={article.author.image} />
+                <img src={data.author.image} />
               </a>
               <div className="info">
                 <a className="author" href={"#@" + authorName}>
                   {authorName}
                 </a>
                 <span className="date">
-                  {new Date(article.createdAt).toLocaleDateString("ko-kr")}
+                  {new Date(data.createdAt).toLocaleDateString("ko-kr")}
                 </span>
               </div>
               <span></span>
@@ -101,10 +61,10 @@ function ArticleDetail() {
           <div className="row article-content">
             <div className="col-xs-12">
               <div>
-                <p>{article.body}</p>
+                <p>{data.body}</p>
               </div>
               <ul className="tag-list">
-                {article.tagList.map((tag) => (
+                {data.tagList.map((tag) => (
                   <li key={tag} className="tag-default tag-pill tag-outline">
                     {tag}
                   </li>
@@ -116,21 +76,7 @@ function ArticleDetail() {
           <div className="article-actions"></div>
           <div className="row">
             <div className="col-xs-12 col-md-8 offset-md-2">
-              <p>
-                <a className="" href="#login">
-                  Sign in
-                </a>
-                &nbsp;or&nbsp;
-                <a className="" href="#register">
-                  sign up
-                </a>
-                &nbsp;to add comments on this article.
-              </p>
-              <div>
-                {commentList?.map((comment) => (
-                  <CommentCard key={comment.id} {...comment} />
-                ))}
-              </div>
+              {slug ? <CommentList slug={slug} /> : null}
             </div>
           </div>
         </div>
